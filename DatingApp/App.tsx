0d25ff31fa0d5eea198d0e11e4fc2e274fe5a43a
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-11-08 03:14:12 +0700

    create project Dating App_Billiards Team

--- DatingApp/App.tsx
+++ DatingApp/App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={require('./assets/images/logo.png')}/>
       <StatusBar style="auto" />
     </View>
   );
