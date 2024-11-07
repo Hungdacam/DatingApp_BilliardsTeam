b5ea964ebd5a20b8b32ee53b2165f6f9555717b7
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-11-08 03:42:47 +0700

    done ui Login

--- DatingApp/App.tsx
+++ DatingApp/App.tsx
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+import { FontAwesome } from '@expo/vector-icons';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/images/logo.png')}/>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/images/logo.png')} style={styles.imageContainer}/>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>
+          HeartSync
+        </Text>
+        <Text style={styles.sloganText}>
+          Where Hearts Connect, Love Finds Its Sync
+        </Text>
+      </View>
+      <View style={styles.buttonLogin}>
+        {/* Apple Button */}
+      <TouchableOpacity style={[styles.button, styles.appleButton]}>
+        <FontAwesome name="apple" size={24} color="white" style={styles.icon} />
+        <Text style={styles.buttonText}>Continue with Apple</Text>
+      </TouchableOpacity>
+
+      {/* Facebook Button */}
+      <TouchableOpacity style={[styles.button, styles.facebookButton]}>
+        <FontAwesome name="facebook" size={24} color="white" style={styles.icon} />
+        <Text style={styles.buttonText}>Continue with Facebook</Text>
+      </TouchableOpacity>
+
+      {/* Phone Number Button */}
+      <TouchableOpacity style={[styles.button, styles.phoneButton]}>
+        <FontAwesome name="phone" size={24} color="white" style={styles.icon} />
+        <Text style={styles.buttonText}>Use phone number</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.privacyContainer}>
+        <Text style={styles.text}>
+          By signing up you agree to our{' '}
+          <Text style={styles.link} >
+            Terms and Conditions
+          </Text>
+        </Text>
+        <Text style={styles.text}>
+          See how we use your data in our{' '}
+          <Text style={styles.link} >
+            Privacy Policy
+          </Text>
+        </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +58,69 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer:{
+    width:250,
+    height:250
+  },
+  titleContainer:{
+    alignItems:'center',
+    marginTop:30
+  },
+  titleText:{
+    fontSize:50,
+    fontWeight:'bold',
+    marginTop:15,
+    marginBottom:5
+  },
+  sloganText:{
+    color:'#A9A9A9',
+    fontSize:17,
+  },
+  buttonLogin:{
+    alignItems:'center',
+    marginTop: 60
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 25,
+    width: 300,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal:20
+  },
+  appleButton: {
+    backgroundColor: '#000000',
+  },
+  facebookButton: {
+    backgroundColor: '#1877F2',
+  },
+  phoneButton: {
+    backgroundColor: '#00BFFF',
+  },
+  privacyContainer:{
+    alignItems: 'center',
+    marginTop: 90,
+  },
+  text: {
+    color: '#555',
+    fontSize: 14,
+    textAlign: 'center',
+    marginVertical: 4,
+  },
+  link: {
+    color: '#007AFF',
+    textDecorationLine: 'underline',
+  },
+  
 });
